20f56967f5d330d8d29f2a1bf9ba5170072630cc
github username <github email address>
2017-09-24 17:36:55 -0400

    books reducer add

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -3,6 +3,6 @@ function(){
     { title: 'Javascript: The Good Parts'},
     { title: 'Harry Potter and the Prisoner of Azkaban'},
     { title: 'The Dark Tower'},
-    { title: 'Eloquent Ruby'},
+    { title: 'Eloquent Ruby'}
   ]
 }
